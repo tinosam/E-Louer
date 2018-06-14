0c448f814ec3c7cfb967dbfdcd96a5bf6a27b358
tinosam <yvestinos@gmail.com>
2018-06-14 13:49:24 +0300

    tino

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'home/index'
-  root 'home#index'
-=======
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, path: 'users'
-devise_for :agents, path: 'agents'
->>>>>>> 1ad33c9ef9891904d5343de8c7ea9a157a813087
+  devise_for :agents, path: 'agents'
+
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
