dcd08c80189f6d10bc769c6a102609f7c8ccea89
tinosam <yvestinos@gmail.com>
2018-06-15 10:40:27 +0300

    tino

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get 'home/contact'
+  get 'home/logement'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
