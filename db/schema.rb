1121c8f9dc94062c6c4360d32827fddd7df470ce
NrjMeyer <nrjmeyer@gmail.com>
2018-06-15 12:12:15 +0300

    base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_071917) do
+ActiveRecord::Schema.define(version: 2018_06_15_081249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,9 @@ ActiveRecord::Schema.define(version: 2018_06_15_071917) do
     t.integer "prix"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "lieux"
+    t.string "equipement"
+    t.string "duree"
   end
 
   create_table "photos", force: :cascade do |t|
