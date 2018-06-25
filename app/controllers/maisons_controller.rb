27b99971317853bf65cf338122cc251b3ec69aeb
NrjMeyer <nrjmeyer@gmail.com>
2018-06-25 12:37:49 +0300

    kely sisa

--- app/controllers/maisons_controller.rb
+++ app/controllers/maisons_controller.rb
@@ -1,30 +1,35 @@
 class MaisonsController < InheritedResources::Base
-  
-    before_action :set_maison, only: [:show, :edit, :update, :destroy]
+
+  before_action :set_maison, only: [:show, :edit, :update, :destroy]
   
     # GET /tranos
     # GET /tranos.json
     def index
-      @q = Maison.ransack(params[:q])
-      @maisons = @q.result
+      if agent_signed_in?
+        maisonss = current_agent.maisons
+        @q = maisonss.ransack(params[:q])
+        @maisons = @q.result
+      else
+        redirect_to home_index_path
+      end
     end
-  
+
     # GET /tranos/1
     # GET /tranos/1.json
     def show
       @photos = @maison.photos.all
     end
-  
+
     # GET /tranos/new
     def new
       @maison = Maison.new
       @photo = @maison.photos.build
     end
-  
+
     # GET /tranos/1/edit
     def edit
     end
-  
+
     # POST /tranos
     # POST /tranos.json
     def create
@@ -35,7 +40,7 @@ class MaisonsController < InheritedResources::Base
       @maison.duree = params[:maison][:duree]
       @maison.equipement = params[:maison][:equipement]
       @maison.prix = params[:maison][:prix].to_i
-  
+
       respond_to do |format|
         if @maison.save
           params[:photos]['image'].each do |a|
@@ -49,7 +54,7 @@ class MaisonsController < InheritedResources::Base
         end
       end
     end
-  
+
     # PATCH/PUT /tranos/1
     # PATCH/PUT /tranos/1.json
     def update
@@ -63,7 +68,7 @@ class MaisonsController < InheritedResources::Base
         end
       end
     end
-  
+
     # DELETE /tranos/1
     # DELETE /tranos/1.json
     def destroy
@@ -73,16 +78,16 @@ class MaisonsController < InheritedResources::Base
         format.json { head :no_content }
       end
     end
-  
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_maison
         @maison = Maison.find(params[:id])
       end
-  
+
       # Never trust parameters from the scary internet, only allow the white list through.
       def maison_params
-        params.require(:photos).permit(:agent_id, photos_attributes: [:id, :maison_id, :image])
+        params.require(:maison).permit(:agent_id, photos_attributes: [:id, :maison_id, :image])
       end
     end
 
